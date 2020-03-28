e598ebc20db22faae51a82128078a13e96bcd81d
iki-sangadji <52664607+iki-sangadji@users.noreply.github.com>
2020-03-28 13:08:09 +0700

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -50,7 +50,15 @@
                                 </div>
                             </div>
                         </div>
-
+                        <div class="login-checkbox">
+                            {{-- <label>
+                                <input type="checkbox" name="remember{{ old('remember') ? 'checked' : '' }}">Remember Me
+                            </label> --}}
+                            <label>
+                                <a href="{{ route('password.request') }}">Lupa kata sandi?</a>
+                                {{csrf_field()}}
+                            </label>
+                        </div>
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
